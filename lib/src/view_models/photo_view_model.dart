27fd2caf3c7d7bdd5736a4a8d5572fc382a9b9c0
Mayan Kelwin <mayankelwin@gmail.com>
2025-07-18 10:37:16 -0400

    feat: Added photo view model

--- lib/src/view_models/photo_view_model.dart
+++ lib/src/view_models/photo_view_model.dart
@@ -1,23 +1,26 @@
 import 'package:flutter/material.dart';
-import '../models/services/photo_service.dart';
 import '../models/core/photo_model.dart';
+import '../models/api/api_services.dart';
 
 class PhotoViewModel extends ChangeNotifier {
-  final _service = PhotoService();
+  final _service = ApiServices();
+
   List<Photo> photos = [];
   bool isLoading = false;
+  String? errorMessage;
 
   Future<void> loadPhotos() async {
     isLoading = true;
+    errorMessage = null;
     notifyListeners();
 
     try {
       photos = await _service.fetchPhotos();
     } catch (e) {
-      print('Erro ao carregar fotos: $e');
+      errorMessage = 'Erro ao carregar fotos: $e';
+    } finally {
+      isLoading = false;
+      notifyListeners();
     }
-
-    isLoading = false;
-    notifyListeners();
   }
 }
